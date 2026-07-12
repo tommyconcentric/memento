827148ec3f4b1751b780c18971074380d1fc5e59
Tommy Le <tommy@concentric.health>
2026-07-12 22:27:18 +0200

    Initial Commit

---
+++ Memento/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memento
+//
+//  Created by Tommy Le on 12/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memento/MementoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoApp.swift
+//  Memento
+//
+//  Created by Tommy Le on 12/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MementoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
